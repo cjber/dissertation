f5d11dabb1557e1a25f12ee2c377d9e55a245a49
cjber <cjberragan@gmail.com>
2019-09-11 15:20:02 +0100

    daily

--- make_all.sh
+++ make_all.sh
@@ -8,3 +8,8 @@ rm -r ./figure/
 
 Rscript -e "knitr::knit2pdf('main.rnw')"
 zathura main.pdf
+
+# remove some unneeded bib stuff
+sed '/^  url/ d' kbib.bib
+sed '/^  note/ d' kbib.bib
+sed '/^  file/ d' kbib.bib
